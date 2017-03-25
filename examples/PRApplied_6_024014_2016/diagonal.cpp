e4df99e13fcafe9bdb9c4d6be9aa9ecf9189ec51
francischen <kfrancischen@gmail.com>
2017-03-24 17:17:26 -0700

    add example for diagonal dielectric

---
+++ examples/PRApplied_6_024014_2016/diagonal.cpp
@@ -0,0 +1,31 @@
+#include "setup.h"
+
+int main(){
+  // initializing simulation
+  Ptr<SimulationPlanar> s = SimulationPlanar::instanceNew();
+  s->addMaterial("hBN", "hBN.txt");
+  s->addMaterial("MCT", "MCT.txt");
+  s->addMaterial("Vacuum", "Vacuum.txt");
+  s->addMaterial("PEC", "PEC.txt");
+ 
+  // add layer from bottom to the top
+  s->addLayer("PECBottom", 0, "PEC");
+  s->addLayer("hBNLayer", 5e-6, "hBN");
+  s->addLayer("VacGap", 1e-8, "Vacuum");
+  s->addLayer("MCTLayer", 5e-6, "MCT");
+  s->addLayerCopy("PECTop", "PECBottom");
+
+  // set source and probe
+  s->setSourceLayer("hBNLayer");
+  s->setProbeLayer("VacGap");
+
+  // set simulation
+  s->setKParallelIntegral(100);
+  s->useQuadgk();
+  s->setOutputFile("output.txt");
+  s->build();
+
+  s->runNaive();
+
+  return 0;
+}
